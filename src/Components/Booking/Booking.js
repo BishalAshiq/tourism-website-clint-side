f5db630da889856a520b58fa71e2c03bbe270e80
BishalAshiq <bishaltheboss20@gmail.com>
2021-10-30 17:56:08 +0600

    booking

--- src/Components/Booking/Booking.js
+++ src/Components/Booking/Booking.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
+import './Booking.css'
 
 const Booking = () => {
-     const {serviceId} = useParams();  
+     const {serviceId} = useParams(); 
+     const [service, setService] = useState({});
+     
+     useEffect(()=>{
+         fetch(`http://localhost:5000/services/${serviceId}`)
+         .then(res=>res.json())
+         .then(data => setService(data))
+     },[])
+     
     return (
         <div>
-            <div>
-                <h2>Booking on this {serviceId}</h2>
+            <div className='booking-service'>
+                <img src={service.img} alt=''/>
+                <h2>{service.name}</h2>
+                <p>{service.description}</p>
             </div>
         </div>
     );
